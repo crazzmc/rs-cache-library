dcba0c6af93b511e10b7e4beba7e809318d1b00c
crazzmc <crazzmc@gmail.com>
2023-05-25 08:37:40 -0400

    model packer cleaned up

--- src/main/kotlin/com/displee/ModelPacker.kt
+++ src/main/kotlin/com/displee/ModelPacker.kt
@@ -1,35 +1,62 @@
 package com.displee
 
 import com.displee.cache.CacheLibrary
+import jdk.internal.org.jline.utils.Colors.s
 import java.io.File
 import java.nio.file.Files
-import java.nio.file.Files.isRegularFile
 import java.nio.file.Path
-import java.nio.file.Paths
-import kotlin.math.exp
 
 class ModelPacker {
     companion object {
 
         val cachePath = System.getProperty("user.dir") + "/cache/"
         val cache = CacheLibrary.create(cachePath)
+        val models = cache.index(7)
+        val modelsPath = System.getProperty("user.dir") + "/models/"
+        var initialOffset = models.archiveIds().last() + 1
+        val exportsPath = System.getProperty("user.dir") + "/exports/"
+
+        val newCachePath = System.getProperty("user.dir") + "/new_cache/" //only used if using the transfer function
+        val newCache = CacheLibrary.create(newCachePath)
+        val newModels = newCache.index(7)
+        var startIndex = 0; //First model to rip from one cache
+        var newCacheStartindex = 0; //Set offset here if you want to move from lets say 42568 but start at 60000
 
         @JvmStatic
         fun main(args: Array<String>) {
-            pack()
+            models.cache()
+//            pack()
 //            get()
 //            export()
 //            replace()
-//            val pack = false
-//            if (pack) pack() else get()
 //            export()
+            transfer()
+        }
+
+        private fun transfer() {
+            newModels.cache()
+            println(
+                "Moving models from old cache to new starting at $startIndex. Size before packing ${
+                    newCache.index(7).archiveIds().size
+                }"
+            )
+            for (id in models.archiveIds()) {
+                val data = cache.data(7, id)
+                if (data != null) {
+                    if (id >= startIndex) {
+                        if (newCacheStartindex > 0) {
+                            newCache.put(7, newCacheStartindex++, data)
+                        } else {
+                            newCache.put(7, startIndex++, data)
+                        }
+                    }
+                }
+            }
+            newModels.update()
+            println("Models packed into new cache, new size: ${newCache.index(7).archiveIds().size}")
         }
 
         private fun pack() {
-            val models = cache.index(7);
-            val modelsPath = System.getProperty("user.dir") + "/models/"
-            models.cache()
-            var initialOffset = models.archiveIds().last() + 1;
             println("Models path $modelsPath")
             File(modelsPath).walk().forEach {
                 if (it.toString().endsWith(".dat")) {
@@ -41,48 +68,34 @@ class ModelPacker {
         }
 
         private fun get() {
-            val models = cache.index(7)
-            models.cache()
             println("Last model id ${models.archiveIds().last()}")
         }
 
-        private fun replace() {
-            val startId = 53915
-            val models = cache.index(7)
-            val modelsPath = System.getProperty("user.dir") + "/models/"
-            models.cache()
-            var initialOffset = startId
+        private fun replace() { //replaces models starting at the startIndex
             println("Models path $modelsPath")
             File(modelsPath).walk().forEach {
                 if (it.toString().endsWith(".dat")) {
-                    println("Packing ${it.name} to index $initialOffset")
-                    cache.put(7, initialOffset++, Files.readAllBytes(it.toPath()))
+                    println("Packing ${it.name} to index $startIndex")
+                    cache.put(7, startIndex++, Files.readAllBytes(it.toPath()))
                 }
             }
             models.update()
         }
 
         private fun export() {
-
-            val modelsPath = System.getProperty("user.dir") + "/exports/"
-
-            val models = cache.index(7)
-            models.cache()
-
             for (id in models.archiveIds()) {
                 val data = cache.data(7, id)
                 if (data != null) {
-                    Files.write(
-                        Path.of(modelsPath + "$id" + ".dat"),
-                        data
-                    );
+                    if (id == 60000) {
+                        Files.write(
+                            Path.of("$exportsPath$id.dat"),
+                            data
+                        )
+                    }
                 }
             }
         }
+
     }
 
 }
-
-//38909
-
-//38937
